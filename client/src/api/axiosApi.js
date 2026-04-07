542f9c477c3d06864a352e5f6e8cacca766dbdf5
Soaib Shaikh <samshaikh1192@gmail.com>
2026-04-07 10:35:08 +0530

    update

--- client/src/api/axiosApi.js
+++ client/src/api/axiosApi.js
@@ -4,6 +4,7 @@ const axiosApi = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", 
 })
 
+
 axiosApi.interceptors.request.use((req)=>{
     const token = localStorage.getItem('token');
     if(token){
