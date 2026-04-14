0a6bfee1dae1d48b5b8b8cd7a1bb4c30b8967001
Soaib Shaikh <samshaikh1192@gmail.com>
2026-04-14 14:24:16 +0530

    change

--- server/configs/mail.js
+++ server/configs/mail.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.PASS_USER,
